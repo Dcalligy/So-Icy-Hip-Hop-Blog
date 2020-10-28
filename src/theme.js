e230a62dae63f763fea5b0026f77c66b8d169165
Dillon Calligy <Dcalligy813@gmail.com>
2020-10-28 17:38:47 -0400

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: "#764abc"
+            main: "#fff"
         }
     },
     background: {
